c926c0dc8721a5ca40846e3195e157ecf403aa1b
Juerg Hunziker <juerg.hunziker@gmail.com>
2014-11-23 19:22:41 +0100

    changed code indenting

--- index.js
+++ index.js
@@ -17,46 +17,46 @@ var bootlint = require('bootlint');
 var PLUGIN_NAME = 'gulp-bootlint';
 
 function gulpBootlint(options) {
-    options = options || {
-        disabledIds: []
+  options = options || {
+    disabledIds: []
+  };
+
+  // creating a stream through which each file will pass
+  var stream = through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      return cb(null, file);
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+      return cb();
+    }
+
+    var reporter = function (lint) {
+      var lintId = (lint.id[0] === 'E') ? chalk.bgRed.white(lint.id) : chalk.bgYellow.white(lint.id),
+          errorElementsAvailable = false;
+
+      if (lint.elements) {
+        lint.elements.each(function (_, element) {
+          var errorLocation = element.startLocation;
+          gutil.log(file.path + ":" + (errorLocation.line + 1) + ":" + (errorLocation.column + 1), lintId, lint.message);
+          errorElementsAvailable = true;
+        });
+      }
+      if (!errorElementsAvailable) {
+        gutil.log(file.path + ":", lintId, lint.message);
+      }
+      file.bootlint.success = false;
+      file.bootlint.issues.push(lint);
     };
 
-    // creating a stream through which each file will pass
-    var stream = through.obj(function (file, enc, cb) {
-        if (file.isNull()) {
-            return cb(null, file);
-        }
+    file.bootlint = { success: true, issues: [] };
+    bootlint.lintHtml(file.contents.toString(), reporter, options.disabledIds);
 
-        if (file.isStream()) {
-            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
-            return cb();
-        }
+    return cb(null, file);
+  });
 
-        var reporter = function (lint) {
-            var lintId = (lint.id[0] === 'E') ? chalk.bgRed.white(lint.id) : chalk.bgYellow.white(lint.id),
-                errorElementsAvailable = false;
-
-            if (lint.elements) {
-                lint.elements.each(function (_, element) {
-                    var errorLocation = element.startLocation;
-                    gutil.log(file.path + ":" + (errorLocation.line + 1) + ":" + (errorLocation.column + 1), lintId, lint.message);
-                    errorElementsAvailable = true;
-                });
-            }
-            if (!errorElementsAvailable) {
-                gutil.log(file.path + ":", lintId, lint.message);
-            }
-            file.bootlint.success = false;
-            file.bootlint.issues.push(lint);
-        };
-
-        file.bootlint = { success: true, issues: [] };
-        bootlint.lintHtml(file.contents.toString(), reporter, options.disabledIds);
-
-        return cb(null, file);
-    });
-
-    return stream;
+  return stream;
 };
 
 // exporting the plugin

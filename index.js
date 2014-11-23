2a70544241203e2d128ee3bdcdc90b4b3778e7f5
Juerg Hunziker <juerg.hunziker@gmail.com>
2014-11-23 19:43:46 +0100

    extended lint logging

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ function gulpBootlint(options) {
 
   // creating a stream through which each file will pass
   var stream = through.obj(function (file, enc, cb) {
+    var errorCount = 0;
+
     if (file.isNull()) {
       return cb(null, file);
     }
@@ -46,15 +48,26 @@ function gulpBootlint(options) {
       if (!errorElementsAvailable) {
         gutil.log(file.path + ":", lintId, lint.message);
       }
+
+      ++errorCount;
       file.bootlint.success = false;
       file.bootlint.issues.push(lint);
     };
 
+    gutil.log(chalk.gray('Linting file ' + file.path));
     file.bootlint = { success: true, issues: [] };
     bootlint.lintHtml(file.contents.toString(), reporter, options.disabledIds);
 
+    if(errorCount > 0) {
+      gutil.log(chalk.red(errorCount + ' lint error(s) found in file ' + file.path));
+    } else {
+      gutil.log(chalk.green(file.path + ' is lint free!'));
+    }
+
     return cb(null, file);
   });
+
+
 
   return stream;
 };

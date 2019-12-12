746e177a897ea99501694a960e2dc1ab6bcf32a3
Juerg Hunziker <juerg.hunziker@gmail.com>
2019-12-12 20:15:56 +0100

    update eslint config

--- index.js
+++ index.js
@@ -14,137 +14,137 @@ var bootlint = require('bootlint');
 // consts
 var PLUGIN_NAME = 'gulp-bootlint';
 
-function gulpBootlint(options) {
-    var hasError = false,
-        hasWarning = false,
-        log, stream;
+function gulpBootlint (options) {
+  var hasError = false;
+  var hasWarning = false;
+  var log, stream;
 
-    /**
-     * Reporter function for linting errors and warnings.
-     *
-     * @param file Current file object.
-     * @param lint Current linting error.
-     * @param isError True if this is an error.
-     * @param isWarning True if this is a warning.
-     * @param errorLocation Error location object.
-     */
-    var defaultReportFn = function(file, lint, isError, isWarning, errorLocation) {
-        var lintId = (isError) ? colors.bgRed(colors.white(lint.id)) : colors.bgYellow(colors.white(lint.id));
-        var message = '';
-        if (errorLocation) {
-            message = file.path + ':' + (errorLocation.line + 1) + ':' + (errorLocation.column + 1) + ' ' + lintId + ' ' + lint.message;
-        } else {
-            message = file.path + ': ' + lintId + ' ' + lint.message;
-        }
+  /**
+   * Reporter function for linting errors and warnings.
+   *
+   * @param file Current file object.
+   * @param lint Current linting error.
+   * @param isError True if this is an error.
+   * @param isWarning True if this is a warning.
+   * @param errorLocation Error location object.
+   */
+  var defaultReportFn = function (file, lint, isError, isWarning, errorLocation) {
+    var lintId = (isError) ? colors.bgRed(colors.white(lint.id)) : colors.bgYellow(colors.white(lint.id));
+    var message = '';
+    if (errorLocation) {
+      message = file.path + ':' + (errorLocation.line + 1) + ':' + (errorLocation.column + 1) + ' ' + lintId + ' ' + lint.message;
+    } else {
+      message = file.path + ': ' + lintId + ' ' + lint.message;
+    }
 
-        if (isError) {
-            log.error(message);
-        } else {
-            log.warning(message);
+    if (isError) {
+      log.error(message);
+    } else {
+      log.warning(message);
+    }
+  };
+
+  /**
+   * Reporter function for linting summary.
+   *
+   * @param file File which was linted.
+   * @param errorCount Total count of errors in file.
+   * @param warningCount Total count of warnings in file.
+   */
+  var defaultSummaryReportFn = function (file, errorCount, warningCount) {
+    if (errorCount > 0 || warningCount > 0) {
+      var message = '';
+      if (errorCount > 0) {
+        message += colors.red(errorCount + ' lint ' + (errorCount === 1 ? 'error' : 'errors'));
+      }
+
+      if (warningCount > 0) {
+        if (errorCount > 0) {
+          message += ' and ';
         }
+        message += colors.yellow(warningCount + ' lint ' + (warningCount === 1 ? 'warning' : 'warnings'));
+      }
+      message += ' found in file ' + file.path;
+      log.notice(message);
+    } else {
+      log.info(colors.green(file.path + ' is lint free!'));
+    }
+  };
+
+  options = merge({
+    stoponerror: false,
+    stoponwarning: false,
+    loglevel: 'error',
+    disabledIds: [],
+    issues: [],
+    reportFn: defaultReportFn,
+    summaryReportFn: defaultSummaryReportFn,
+  }, options);
+
+  log = new Log(options.loglevel);
+
+  // creating a stream through which each file will pass
+  stream = through.obj(function (file, enc, cb) {
+    var errorCount = 0;
+    var warningCount = 0;
+
+    if (file.isNull()) {
+      return cb(null, file);
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+      return cb();
+    }
+
+    var reporter = function (lint) {
+      var isError = (lint.id[0] === 'E');
+      var isWarning = (lint.id[0] === 'W');
+      var errorElementsAvailable = false;
+
+      if (lint.elements) {
+        lint.elements.each(function (_, element) {
+          if (options.reportFn) {
+            options.reportFn(file, lint, isError, isWarning, element.startLocation);
+          }
+          errorElementsAvailable = true;
+        });
+      }
+      if (!errorElementsAvailable && options.reportFn) {
+        options.reportFn(file, lint, isError, isWarning, null);
+      }
+
+      if (isError) {
+        ++errorCount;
+        hasError = true;
+      }
+      if (isWarning) {
+        ++warningCount;
+        hasWarning = true;
+      }
+      options.issues.push(lint);
     };
 
-    /**
-     * Reporter function for linting summary.
-     *
-     * @param file File which was linted.
-     * @param errorCount Total count of errors in file.
-     * @param warningCount Total count of warnings in file.
-     */
-    var defaultSummaryReportFn = function(file, errorCount, warningCount) {
-        if (errorCount > 0 || warningCount > 0) {
-            var message = '';
-            if (errorCount > 0) {
-                message += colors.red(errorCount + ' lint ' + (errorCount === 1 ? 'error' : 'errors'));
-            }
+    log.info(colors.gray('Linting file ' + file.path));
+    bootlint.lintHtml(file.contents.toString(), reporter, options.disabledIds);
 
-            if (warningCount > 0) {
-                if (errorCount > 0) {
-                    message += ' and ';
-                }
-                message += colors.yellow(warningCount + ' lint ' + (warningCount === 1 ? 'warning' : 'warnings'));
-            }
-            message += ' found in file ' + file.path;
-            log.notice(message);
-        } else {
-            log.info(colors.green(file.path + ' is lint free!'));
-        }
-    };
+    if (options.summaryReportFn) {
+      options.summaryReportFn(file, errorCount, warningCount);
+    }
 
-    options = merge({
-        stoponerror: false,
-        stoponwarning: false,
-        loglevel: 'error',
-        disabledIds: [],
-        issues: [],
-        reportFn: defaultReportFn,
-        summaryReportFn: defaultSummaryReportFn
-    }, options);
+    return cb(null, file);
+  }, function (cb) {
+    if (hasError && options.stoponerror) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Lint errors found!'));
+    }
+    if (hasWarning && options.stoponwarning) {
+      this.emit('warning', new PluginError(PLUGIN_NAME, 'Lint warnings found!'));
+    }
 
-    log = new Log(options.loglevel);
+    return cb();
+  });
 
-    // creating a stream through which each file will pass
-    stream = through.obj(function (file, enc, cb) {
-        var errorCount = 0,
-            warningCount = 0;
-
-        if (file.isNull()) {
-            return cb(null, file);
-        }
-
-        if (file.isStream()) {
-            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
-            return cb();
-        }
-
-        var reporter = function (lint) {
-            var isError = (lint.id[0] === 'E'),
-                isWarning = (lint.id[0] === 'W'),
-                errorElementsAvailable = false;
-
-            if (lint.elements) {
-                lint.elements.each(function (_, element) {
-                    if(options.reportFn){
-                        options.reportFn(file, lint, isError, isWarning, element.startLocation);
-                    }
-                    errorElementsAvailable = true;
-                });
-            }
-            if (!errorElementsAvailable && options.reportFn) {
-                options.reportFn(file, lint, isError, isWarning, null);
-            }
-
-            if (isError) {
-                ++errorCount;
-                hasError = true;
-            }
-            if (isWarning) {
-                ++warningCount;
-                hasWarning = true;
-            }
-            options.issues.push(lint);
-        };
-
-        log.info(colors.gray('Linting file ' + file.path));
-        bootlint.lintHtml(file.contents.toString(), reporter, options.disabledIds);
-
-        if(options.summaryReportFn){
-            options.summaryReportFn(file, errorCount, warningCount);
-        }
-
-        return cb(null, file);
-    }, function (cb) {
-        if (hasError && options.stoponerror ) {
-            this.emit('error', new PluginError(PLUGIN_NAME, 'Lint errors found!'));
-        }
-        if (hasWarning && options.stoponwarning) {
-            this.emit('warning', new PluginError(PLUGIN_NAME, 'Lint warnings found!'));
-        }
-
-        return cb();
-    });
-
-    return stream;
+  return stream;
 }
 
 // exporting the plugin
--- test/test.js
+++ test/test.js
@@ -6,8 +6,8 @@
  * Licensed under the MIT license.
  */
 
-/*eslint no-unused-vars: "off"*/
-/*global describe, it*/
+/* eslint no-unused-vars: "off" */
+/* global describe, it */
 
 var bootlintPlugin = require('../');
 var should = require('should');
@@ -15,248 +15,248 @@ var Vinyl = require('vinyl');
 var fs = require('fs');
 var path = require('path');
 
-var getFile = function(filePath) {
-    var fullFilePath = './test/' + filePath;
-    return new Vinyl({
-        path: fullFilePath,
-        cwd: './test/',
-        base: path.dirname(fullFilePath),
-        contents: fs.readFileSync(fullFilePath)
-    });
+var getFile = function (filePath) {
+  var fullFilePath = './test/' + filePath;
+  return new Vinyl({
+    path: fullFilePath,
+    cwd: './test/',
+    base: path.dirname(fullFilePath),
+    contents: fs.readFileSync(fullFilePath),
+  });
 };
 
-describe('gulp-bootlint', function() {
-    describe('bootlintPlugin', function() {
-        it('should pass file through', function(done) {
-            var file = getFile('fixtures/valid-bootstrap.html'),
-                stream = bootlintPlugin(),
-                fileCount = 0;
+describe('gulp-bootlint', function () {
+  describe('bootlintPlugin', function () {
+    it('should pass file through', function (done) {
+      var file = getFile('fixtures/valid-bootstrap.html');
+      var stream = bootlintPlugin();
+      var fileCount = 0;
 
-            stream.on('data', function(file) {
-                should.exist(file);
-                should.exist(file.path);
-                should.exist(file.relative);
-                should.exist(file.contents);
-                file.path.should.equal(path.normalize('./test/fixtures/valid-bootstrap.html'));
-                file.relative.should.equal('valid-bootstrap.html');
-                ++fileCount;
-            });
+      stream.on('data', function (file) {
+        should.exist(file);
+        should.exist(file.path);
+        should.exist(file.relative);
+        should.exist(file.contents);
+        file.path.should.equal(path.normalize('./test/fixtures/valid-bootstrap.html'));
+        file.relative.should.equal('valid-bootstrap.html');
+        ++fileCount;
+      });
 
-            stream.once('end', function() {
-                fileCount.should.equal(1);
-                done();
-            });
+      stream.once('end', function () {
+        fileCount.should.equal(1);
+        done();
+      });
 
-            stream.write(file);
-            stream.end();
-        });
-
-        it('should lint valid file', function(done) {
-            var file = getFile('fixtures/valid-bootstrap.html'),
-                issues = [],
-                stream = bootlintPlugin({
-                    issues: issues
-                });
-
-            stream.on('data', function(file) {
-                should.exist(file);
-                issues.length.should.equal(0);
-            });
-
-            stream.once('end', function() {
-                done();
-            });
-
-            stream.write(file);
-            stream.end();
-        });
-
-        it('should find errors in invalid file', function(done) {
-            var file = getFile('fixtures/error-bootstrap.html'),
-                issues = [],
-                stream = bootlintPlugin({
-                    issues: issues
-                });
-
-            stream.on('data', function() {
-                issues.length.should.equal(1);
-                issues[0].id.should.equal('E001');
-            });
-            stream.once('end', function() {
-                done();
-            });
-
-            stream.write(file);
-            stream.end();
-        });
-
-        it('should find warnings in invalid file', function(done) {
-            var file = getFile('fixtures/warning-bootstrap.html'),
-                issues = [],
-                stream = bootlintPlugin({
-                    issues: issues
-                });
-
-            stream.on('data', function(file) {
-                issues.length.should.equal(1);
-                issues[0].id.should.equal('W009');
-            });
-            stream.once('end', function() {
-                done();
-            });
-
-            stream.write(file);
-            stream.end();
-        });
-
-        it('should find errors and warnings in invalid file', function(done) {
-            var file = getFile('fixtures/error_warning-bootstrap.html'),
-                issues = [],
-                stream = bootlintPlugin({
-                    issues: issues
-                });
-
-            stream.on('data', function(file) {
-                issues.length.should.equal(2);
-                issues[0].id.should.equal('E001');
-                issues[1].id.should.equal('W009');
-            });
-            stream.once('end', function() {
-                done();
-            });
-
-            stream.write(file);
-            stream.end();
-        });
-
-        it('should stop on warnings', function(done) {
-            var file = getFile('fixtures/warning-bootstrap.html'),
-                stream = bootlintPlugin({
-                    stoponwarning: true
-                });
-
-            stream.on('warning', function( err ) {
-                err.message.should.equal('Lint warnings found!');
-                done();
-            });
-
-            stream.write(file);
-            stream.end();
-        });
-
-        it('should stop on errors', function(done) {
-            var file = getFile('fixtures/error-bootstrap.html'),
-                stream = bootlintPlugin({
-                    stoponerror: true
-                });
-
-            stream.on('error', function( err ) {
-                err.message.should.equal('Lint errors found!');
-                done();
-            });
-
-            stream.write(file);
-            stream.end();
-        });
-
-        it('should stop if there are errors and warnings', function(done) {
-            var file = getFile('fixtures/error_warning-bootstrap.html'),
-                stream = bootlintPlugin({
-                    stoponwarning: true,
-                    stoponerror: true
-                });
-
-            stream.on('error', function( err ) {
-                err.message.should.equal('Lint errors found!');
-                done();
-            });
-
-            stream.write(file);
-            stream.end();
-        });
-
-        it('should not fail on disabled ids', function(done) {
-            var file = getFile('fixtures/error_warning-bootstrap.html'),
-                issues = [],
-                stream = bootlintPlugin({
-                    disabledIds: ['E001', 'W009'],
-                    issues: issues
-                });
-
-            stream.on('data', function(file) {
-                issues.length.should.equal(0);
-            });
-
-            stream.once('end', function() {
-                done();
-            });
-
-            stream.write(file);
-            stream.end();
-        });
-
-        it('should use own report function', function(done) {
-            var lintResults = {
-                file: null,
-                lint: null,
-                isError: false,
-                isWarning: false
-            };
-            var myReportFn = function(file, lint, isError, isWarning, errorLocation) {
-                lintResults.file = file;
-                lintResults.lint = lint;
-                lintResults.isError = isError;
-                lintResults.isWarning = isWarning;
-            };
-            var file = getFile('fixtures/error-bootstrap.html'),
-                stream = bootlintPlugin({
-                    reportFn: myReportFn
-                });
-
-            stream.on('data', function(file) {
-                should.equal(lintResults.file, file);
-                should.exist(lintResults.lint);
-                should.equal(lintResults.lint.id, 'E001');
-                should.equal(lintResults.isError, true);
-                should.equal(lintResults.isWarning, false);
-            });
-
-            stream.once('end', function() {
-                done();
-            });
-
-            stream.write(file);
-            stream.end();
-        });
-
-        it('should use own summary report function', function(done) {
-            var lintResults = {
-                file: null,
-                errorCount: 0,
-                warningCount: 0
-            };
-            var mySummaryReportFn = function(file, errorCount, warningCount) {
-                lintResults.file = file;
-                lintResults.errorCount = errorCount;
-                lintResults.warningCount = warningCount;
-            };
-            var file = getFile('fixtures/error-bootstrap.html'),
-                stream = bootlintPlugin({
-                    summaryReportFn: mySummaryReportFn
-                });
-
-            stream.on('data', function(file) {
-                should.equal(lintResults.file, file);
-                should.equal(lintResults.errorCount, 1);
-                should.equal(lintResults.warningCount, 0);
-            });
-
-            stream.once('end', function() {
-                done();
-            });
-
-            stream.write(file);
-            stream.end();
-        });
+      stream.write(file);
+      stream.end();
     });
+
+    it('should lint valid file', function (done) {
+      var file = getFile('fixtures/valid-bootstrap.html');
+      var issues = [];
+      var stream = bootlintPlugin({
+        issues: issues,
+      });
+
+      stream.on('data', function (file) {
+        should.exist(file);
+        issues.length.should.equal(0);
+      });
+
+      stream.once('end', function () {
+        done();
+      });
+
+      stream.write(file);
+      stream.end();
+    });
+
+    it('should find errors in invalid file', function (done) {
+      var file = getFile('fixtures/error-bootstrap.html');
+      var issues = [];
+      var stream = bootlintPlugin({
+        issues: issues,
+      });
+
+      stream.on('data', function () {
+        issues.length.should.equal(1);
+        issues[0].id.should.equal('E001');
+      });
+      stream.once('end', function () {
+        done();
+      });
+
+      stream.write(file);
+      stream.end();
+    });
+
+    it('should find warnings in invalid file', function (done) {
+      var file = getFile('fixtures/warning-bootstrap.html');
+      var issues = [];
+      var stream = bootlintPlugin({
+        issues: issues,
+      });
+
+      stream.on('data', function (file) {
+        issues.length.should.equal(1);
+        issues[0].id.should.equal('W009');
+      });
+      stream.once('end', function () {
+        done();
+      });
+
+      stream.write(file);
+      stream.end();
+    });
+
+    it('should find errors and warnings in invalid file', function (done) {
+      var file = getFile('fixtures/error_warning-bootstrap.html');
+      var issues = [];
+      var stream = bootlintPlugin({
+        issues: issues,
+      });
+
+      stream.on('data', function (file) {
+        issues.length.should.equal(2);
+        issues[0].id.should.equal('E001');
+        issues[1].id.should.equal('W009');
+      });
+      stream.once('end', function () {
+        done();
+      });
+
+      stream.write(file);
+      stream.end();
+    });
+
+    it('should stop on warnings', function (done) {
+      var file = getFile('fixtures/warning-bootstrap.html');
+      var stream = bootlintPlugin({
+        stoponwarning: true,
+      });
+
+      stream.on('warning', function (err) {
+        err.message.should.equal('Lint warnings found!');
+        done();
+      });
+
+      stream.write(file);
+      stream.end();
+    });
+
+    it('should stop on errors', function (done) {
+      var file = getFile('fixtures/error-bootstrap.html');
+      var stream = bootlintPlugin({
+        stoponerror: true,
+      });
+
+      stream.on('error', function (err) {
+        err.message.should.equal('Lint errors found!');
+        done();
+      });
+
+      stream.write(file);
+      stream.end();
+    });
+
+    it('should stop if there are errors and warnings', function (done) {
+      var file = getFile('fixtures/error_warning-bootstrap.html');
+      var stream = bootlintPlugin({
+        stoponwarning: true,
+        stoponerror: true,
+      });
+
+      stream.on('error', function (err) {
+        err.message.should.equal('Lint errors found!');
+        done();
+      });
+
+      stream.write(file);
+      stream.end();
+    });
+
+    it('should not fail on disabled ids', function (done) {
+      var file = getFile('fixtures/error_warning-bootstrap.html');
+      var issues = [];
+      var stream = bootlintPlugin({
+        disabledIds: ['E001', 'W009'],
+        issues: issues,
+      });
+
+      stream.on('data', function (file) {
+        issues.length.should.equal(0);
+      });
+
+      stream.once('end', function () {
+        done();
+      });
+
+      stream.write(file);
+      stream.end();
+    });
+
+    it('should use own report function', function (done) {
+      var lintResults = {
+        file: null,
+        lint: null,
+        isError: false,
+        isWarning: false,
+      };
+      var myReportFn = function (file, lint, isError, isWarning, errorLocation) {
+        lintResults.file = file;
+        lintResults.lint = lint;
+        lintResults.isError = isError;
+        lintResults.isWarning = isWarning;
+      };
+      var file = getFile('fixtures/error-bootstrap.html');
+      var stream = bootlintPlugin({
+        reportFn: myReportFn,
+      });
+
+      stream.on('data', function (file) {
+        should.equal(lintResults.file, file);
+        should.exist(lintResults.lint);
+        should.equal(lintResults.lint.id, 'E001');
+        should.equal(lintResults.isError, true);
+        should.equal(lintResults.isWarning, false);
+      });
+
+      stream.once('end', function () {
+        done();
+      });
+
+      stream.write(file);
+      stream.end();
+    });
+
+    it('should use own summary report function', function (done) {
+      var lintResults = {
+        file: null,
+        errorCount: 0,
+        warningCount: 0,
+      };
+      var mySummaryReportFn = function (file, errorCount, warningCount) {
+        lintResults.file = file;
+        lintResults.errorCount = errorCount;
+        lintResults.warningCount = warningCount;
+      };
+      var file = getFile('fixtures/error-bootstrap.html');
+      var stream = bootlintPlugin({
+        summaryReportFn: mySummaryReportFn,
+      });
+
+      stream.on('data', function (file) {
+        should.equal(lintResults.file, file);
+        should.equal(lintResults.errorCount, 1);
+        should.equal(lintResults.warningCount, 0);
+      });
+
+      stream.once('end', function () {
+        done();
+      });
+
+      stream.write(file);
+      stream.end();
+    });
+  });
 });
